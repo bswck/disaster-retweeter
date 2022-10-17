36871cc8e12556e9f7264821987d77a5eef08d33
bswck <bswck.dev@gmail.com>
2022-10-18 00:58:54 +0200

    Make db subfolders packages

---
+++ retweeter/db/persistent_log/__init__.py
@@ -0,0 +1,11 @@
+from . import crud, database, models, schemas
+
+from loguru import logger
+
+
+def setup():
+    from .database import Base, engine
+    logger.info("Creating the persistent log database...")
+    Base.metadata.create_all(bind=engine)
+    logger.success("Persistent log database has or had been created")
+
