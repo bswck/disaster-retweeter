5064e18a9c38483f693ce7664f5c77cdd7d771c8
bswck <bswck.dev@gmail.com>
2022-10-18 18:30:33 +0200

    Fill in unmerged reqs, default to empty strings from None when updating env

--- scripts/test_stage.py
+++ scripts/test_stage.py
@@ -21,13 +21,13 @@ def setup_test_stage():
     # monkeypatch production environment variables for testing purposes
     # 0% risk: production environment variables are overridden
     os.environ.update(
-        PERSISTENT_LOG_DB_USERNAME=os.getenv("TEST_PERSISTENT_LOG_DB_USERNAME"),
-        PERSISTENT_LOG_DB_PASSWORD=os.getenv("TEST_PERSISTENT_LOG_DB_PASSWORD"),
-        PERSISTENT_LOG_DB_HOST=os.getenv("TEST_PERSISTENT_LOG_DB_HOST"),
+        PERSISTENT_LOG_DB_USERNAME=os.getenv("TEST_PERSISTENT_LOG_DB_USERNAME") or "",
+        PERSISTENT_LOG_DB_PASSWORD=os.getenv("TEST_PERSISTENT_LOG_DB_PASSWORD") or "",
+        PERSISTENT_LOG_DB_HOST=os.getenv("TEST_PERSISTENT_LOG_DB_HOST") or "",
         PERSISTENT_LOG_DB_PORT=os.getenv("TEST_PERSISTENT_LOG_DB_PORT", "5432"),
-        ANALYTICS_DB_USERNAME=os.getenv("TEST_ANALYTICS_DB_USERNAME"),
-        ANALYTICS_DB_PASSWORD=os.getenv("TEST_ANALYTICS_DB_PASSWORD"),
-        ANALYTICS_DB_HOST=os.getenv("TEST_ANALYTICS_DB_HOST"),
+        ANALYTICS_DB_USERNAME=os.getenv("TEST_ANALYTICS_DB_USERNAME") or "",
+        ANALYTICS_DB_PASSWORD=os.getenv("TEST_ANALYTICS_DB_PASSWORD") or "",
+        ANALYTICS_DB_HOST=os.getenv("TEST_ANALYTICS_DB_HOST") or "",
         ANALYTICS_DB_PORT=os.getenv("TEST_ANALYTICS_DB_PORT", "6379"),
     )
 
